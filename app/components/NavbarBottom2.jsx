ed53615a9e32cb1574b2ba614dca52138ddc0f67
Zoran Dasic <zorandsc@yahoo.com>
2024-04-28 14:02:21 +0200

    fixsing navbar2

--- app/components/NavbarBottom2.jsx
+++ app/components/NavbarBottom2.jsx
@@ -10,24 +10,24 @@ const NavbarBottom2 = () => {
   console.log("pathname", pathname)
   return (
     <nav className="nav2">
-      <menu>
-        <li className={`${pathname === '/' ? 'active' : ''}`}>
+      <menu className="menu2">
+        <li className={`link ${pathname === '/' ? 'active' : ''}`}>
           <Link  href="/" >
             <i className="fa fa-home" aria-hidden="true"></i>
           </Link>
         </li>
-        <li className={`${pathname === '/users' ? 'active' : ''}`}>
+        <li className={`link ${pathname === '/users' ? 'active' : ''}`}>
           <Link href="/users">
             <i className="fa fa-users" aria-hidden="true"></i>
           </Link>
         </li>
         
-        <li className={`${pathname === '/likes' ? 'active' : ''}`}>
+        <li className={`link ${pathname === '/likes' ? 'active' : ''}`}>
           <Link href="/likes">
             <i className="fa fa-heart" aria-hidden="true"></i>
           </Link>
         </li>
-        <li className={`${pathname === '/user' ? 'active' : ''}`}>
+        <li className={`link ${pathname === '/user' ? 'active' : ''}`}>
           <Link href="/user">
             <i className="fa fa-user-circle" aria-hidden="true"></i>
           </Link>
